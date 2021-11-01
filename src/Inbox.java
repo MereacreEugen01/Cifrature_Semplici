12839f37bfc3dec0cfdc276e139a0bb1f99ae799
LenziFilippo04 <lenzi.filippo@studenti-ittfedifermi.edu.it>
2021-11-01 13:04:23 +0100

    Inserimento parziale socket nel secret inbox

--- src/Inbox.java
+++ src/Inbox.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 import java.util.ArrayList;
 public class Inbox extends JFrame {
 
@@ -27,6 +31,9 @@ public class Inbox extends JFrame {
 	JPanel nord = new JPanel();
 
 	JComboBox tendina = new JComboBox();
+	
+	private DatagramSocket socket;
+	private DatagramPacket p;
 
 	private ArrayList<String> listaMessaggi = new ArrayList();
 	public Inbox() {
@@ -94,6 +101,68 @@ public class Inbox extends JFrame {
 	public void decifraVigenere(String messaggio, int chiave) {
 
 	}
+	
+	public void ricezioneMessaggi(int port) 
+	{ 
+		try 
+		{
+			//Creazione socket sulla porta che diamo in input al nostro Secret Inbox
+			socket = new DatagramSocket(port);
+
+			Thread t = new Thread() 
+			{
+				@Override
+				public void run() 
+				{
+					Thread t = new Thread() 
+					{
+						@Override
+						public void run() 
+						{
+							boolean corretto = true;
+							while(corretto) 
+							{
+								byte[] buf = new byte[100];
+								p = new DatagramPacket(buf, 0, buf.length);
+								try 
+								{
+									//Ricezione messaggio
+									socket.receive(p);
+									
+								} 
+								catch (IOException e) 
+								{
+									e.printStackTrace();
+								}
+								String msg = new String(p.getData(), 0, p.getLength());
+								System.out.println(msg);
+								//Controllo se il messaggio arrivato è uguale a "pronto" , se si cambio il messaggio in "pronto!"
+								if(msg.trim().equals("pronto")) 
+								{
+									msg = "pronto!";
+								}else {
+									System.out.println(msg); //Stampa del messaggio appena arrivato
+									listaMessaggi.add(msg);  //Aggiungo il messaggio arrivato alla lista dei messaggi
+								}
+							}
+						}
+							
+						};
+						t.start();
+					}
+					
+				};
+				t.start();
+				
+			} 
+			catch (SocketException e) 
+			{
+				JOptionPane.showMessageDialog(null, "Non è stato possibile aprire la socket", "Attenzione", JOptionPane.WARNING_MESSAGE);
+			}
+			
+		}
+				
+								
 	public void actionPerformed(ActionEvent listener) {	
 		int chiave;
 		if(bottone1==listener.getSource() && scelta1.isSelected() && eUnNumero(casella.getText())) {
