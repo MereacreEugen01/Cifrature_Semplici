1a82fa4289169c3da2981fa40d6afa59d2ee7ffb
MereacreEugen01 <mereacre.eugen@studenti-ittfedifermi.edu.it>
2021-11-10 11:57:26 +0100

    continuo sviluppo interfaccia grafica secret sender

--- src/Sender.java
+++ src/Sender.java
@@ -1,22 +1,19 @@
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class Sender extends JFrame implements ActionListener
 {
 
-	private JPanel pannelloPrincipale, pannelloCentrale;
+	private JPanel pannelloPrincipale, pannelloCentrale, pannelloNord, pannelloSud, pannellOvest;
 	private static final long serialVersionUID = 1L;
-	private int altezza= 200, larghezza= 630;//dimensioni della finestra
+	private int altezza= 400, larghezza= 800;//dimensioni della finestra
 	private Dimension dim = Toolkit.getDefaultToolkit().getScreenSize(); //variabile di supporto per posizionare la finestra al centro dello schermo
 	private int x =  (dim.width-larghezza)/2, y = (dim.height-altezza)/2;//coordinate x e y per posizionare la finestra in maniera dinamica
 	private JTextField codiceAgente;
+	private JLabel titolo, sottoTitolo, cifratura, idAgente; 
+	private JTextArea  messaggio;
 
 	
 	public Sender()
@@ -25,17 +22,66 @@ public class Sender extends JFrame implements ActionListener
 		setBounds(x, y, larghezza, altezza);
 		pannelloPrincipale = (JPanel)getContentPane();
 		pannelloCentrale = new JPanel();
-		pannelloCentrale.setLayout(null);
+		pannelloCentrale.setLayout(new FlowLayout());
+		
+		
+		//pannelloCentrale.add(codiceAgente);
+		
+		titolo = new JLabel("Secret Sender");
+		titolo.setHorizontalAlignment(JLabel.CENTER);
+		sottoTitolo = new JLabel("Mereacre Eugen & Lenzi Filippo 5IA");
+		sottoTitolo.setHorizontalAlignment(JLabel.CENTER);
+		
+		pannelloNord = new JPanel();
+		pannelloNord.setLayout(new GridLayout(2,1));
+		pannelloNord.add(titolo);
+		pannelloNord.add(sottoTitolo);
+		
+		pannellOvest = new JPanel();
+		pannellOvest.setLayout(new BoxLayout(pannellOvest, BoxLayout.Y_AXIS));
+		
+		FlowLayout flow = new FlowLayout();
+		flow.setVgap(2);
+		flow.setAlignment(FlowLayout.LEFT);
+		
+		JPanel sottoPannello1 = new JPanel();
+		sottoPannello1.setLayout(flow);
+		sottoPannello1.add(new JLabel("Codice agente:"));
 		
 		codiceAgente = new JTextField();
-		codiceAgente.setBounds(20,20, 40, 20 );
-		codiceAgente.setVisible(true);
-		pannelloCentrale.add(codiceAgente);
+		codiceAgente.setPreferredSize(new Dimension(40,20));
+		JPanel sottoPannello2 = new JPanel();
+		sottoPannello2.setLayout(flow);
+		sottoPannello2.add(codiceAgente);
+		
+		JPanel sottoPannello3 = new JPanel();
+		sottoPannello3.setLayout(flow);
+		sottoPannello3.add(new JLabel("Messaggio:"));
+		
+		messaggio = new JTextArea();
+		messaggio.setPreferredSize(new Dimension(400,200));
+		messaggio.setLineWrap(true);
+		JScrollPane scrollBar = new JScrollPane(messaggio);
+		JPanel sottoPannello4 = new JPanel();
+		sottoPannello4.setLayout(flow);
+		sottoPannello4.add(scrollBar);	
+		
+		pannellOvest.add(sottoPannello1);
+		pannellOvest.add(sottoPannello2);
+		pannellOvest.add(sottoPannello3);
+		pannellOvest.add(sottoPannello4);
+		
+		JPanel pannelloSinistra = new JPanel();
+		pannelloSinistra.add(pannellOvest);
+	
+		pannelloPrincipale.add(pannelloCentrale, BorderLayout.CENTER);
+		pannelloPrincipale.add(pannelloNord, BorderLayout.NORTH);
+		pannelloPrincipale.add(pannelloSinistra, BorderLayout.WEST);
 		
 		
-		pannelloPrincipale.add(pannelloCentrale);
 		
-			
+		
+		setResizable(false);	
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	}
